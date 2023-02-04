b506a193eb7a35527785a9b10f93f7b2907713c6
santiagosimonsantos <santiago.simon.santos@gmail.com>
2023-02-04 20:34:11 +0100

    my first commit

---
+++ pages/components/Card.jsx
@@ -0,0 +1,53 @@
+import { useRef } from "react"
+
+export const Card = ({ title, description, icon, color }) => {
+    const wrapper = useRef(null)
+    const card = useRef(null)
+
+    const handleMouseMove = (e) => {
+        e.preventDefault()
+        card.current.style.transition = "none"
+        const { width, height, left, top } =
+            wrapper.current.getBoundingClientRect()
+        const x = e.clientX - left
+        const y = e.clientY - top
+        const halfWidth = width / 2
+        const halfHeight = height / 2
+        const rotationX = ((y - halfHeight) / halfHeight) * 10
+        const rotationY = ((x - halfWidth) / halfWidth) * 10
+        card.current.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`
+    }
+
+    const handleMouseLeave = () => {
+        card.current.style.transition = "transform 0.5s ease-in-out"
+        card.current.style.transform = "rotateX(0) rotateY(0)"
+    }
+
+    return (
+        <div
+            className="[perspective:1000px] hover:z-50"
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            ref={wrapper}
+        >
+            <div
+                className={`${color} cursor-pointer overflow-hidden relative block max-w-xl p-6  border-gray-200 rounded-md shadow dark:border-red-700`}
+                ref={card}
+            >
+                <div>
+                    <div className="relative z-10">
+                        <h5 className="mb-2 text-4xl  font-bold tracking-tight text-gray-900 dark:text-white">
+                            {title}
+                        </h5>
+                        <p className="font-normal text-justify bg- text-gray-700 dark:text-white text-lg p-2">
+                            {description}
+                        </p>
+                    </div>
+                    <div className="absolute opacity-30 -bottom-8 -left-16 contrast-200">
+                        {icon}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ pages/components/CardDeck.js
@@ -0,0 +1,28 @@
+import { Nodejs } from "./logos/Nodejs"
+import { Unity } from "./logos/Unity"
+import { Rootme } from "./logos/Rootme"
+
+export const CardDeck = [
+    {
+        theme: 'Nodejs',
+        Icon: Nodejs,
+        title: "FullStack",
+        description: "Desarrollo de aplicaciones web y móviles",
+        colors: "bg-emerald-50 dark:bg-emerald-300"
+    },
+    {
+        theme: 'Rootme',
+        Icon: Rootme,
+        title: "Ciberseguridad",
+        description: "Grupo autodidacta de ciberseguridad orientado a aprendizaje autodidacta a través de las diferentes plataformas, partiendo de lo más básico hasta la más avanzado con el objetivo de presentarse a CTFSs",
+        colors: "bg-red-50 dark:bg-red-400"
+    },
+    {
+        theme: 'Unity',
+        Icon: Unity,
+        title: "Desarrollo de videojuegos",
+        description: "Desarrollo de aplicaciones web y móviles",
+        colors: "bg-zinc-50 dark:bg-zinc-400"
+    }
+
+]
